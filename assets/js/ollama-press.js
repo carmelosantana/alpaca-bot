141faa04f892b4aa769885a1649980622f5bfee3
Carmelo Santana <me@carmelosantana.com>
2024-02-02 11:40:32 -0500

    Add copyToClipboard()
    Add fadeOut()
    Add resubmitPrompt()
    Add check for message element before starting chat functions

--- assets/js/ollama-press.js
+++ assets/js/ollama-press.js
@@ -82,6 +82,74 @@ function onClickChange() {
     copyMessage();
 }
 
+function copyToClipboard(id) {
+    // build vars for response and button using id
+    var btn = document.getElementById('copy-' + id);
+
+    // copy response to clipboard
+    var copyText = getResponseInnerHTML(id);
+
+    // clean up the response
+    copyText = copyText.replace(/<zero-md><script type="text\/markdown">/g, '');
+    copyText = copyText.replace(/<\/script><\/zero-md>/g, '');
+    copyText = copyText.trim();
+
+    // copy and alert user
+    navigator.clipboard.writeText(copyText).then(() => {
+        // change button text
+        btn.innerHTML = 'inventory';
+
+        // After sleep of 5 seconds, change button text back to copy
+        setTimeout(function () {
+            btn.innerHTML = 'content_paste';
+        }, 2600);
+    });
+}
+
+// JS to append fadeout CSS class to the element after timeout 
+function fadeOut(id, timeout = 2600, class_name = 'fadeOut') {
+    var element = document.getElementById(id);
+
+    // After timeout, add the class to the element
+    setTimeout(function () {
+        element.classList.add(class_name);
+    }, timeout);
+}
+
+function getResponseInnerHTML(id) {
+    // if id doesn't start with response-, add response-
+    if (!id.startsWith('response-')) {
+        id = 'response-' + id;
+    }
+
+    var response = document.getElementById(id);
+
+    // copy response to clipboard
+    var html = response.innerHTML;
+
+    // clean up the response
+    html = html.replace(/<zero-md><script type="text\/markdown">/g, '');
+    html = html.replace(/<\/script><\/zero-md>/g, '');
+    html = html.trim();
+
+    return html;
+}
+
+function resubmitPrompt(id) {
+    // get response innerHTML
+    var html = getResponseInnerHTML(id);
+
+    // set prompt value to response innerHTML
+    prompt.value = html;
+    message.value = prompt.value;
+
+    // Scroll to indicator
+    smoothScrollTo('loading');
+
+    // Submit form
+    onClickChange();
+}
+
 // find element by class and scroll to it, parameter is the class name
 function smoothScrollTo(selector = "dialog", behavior = 'smooth', block = 'start') {
     switch (selector) {
@@ -117,45 +185,48 @@ function smoothScrollTo(selector = "dialog", behavior = 'smooth', block = 'start
     console.log(element.id);
 }
 
-// If the Enter key is pressed without Shift and the window width is large "enough"
-message.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 640) {
-        e.preventDefault();
-        if (!isBlank(message.value)) {
-            submit.click();
+if (message) {
+
+    // If the Enter key is pressed without Shift and the window width is large "enough"
+    message.addEventListener("keydown", (e) => {
+        if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 640) {
+            e.preventDefault();
+            if (!isBlank(message.value)) {
+                submit.click();
+            }
+        };
+    });
+
+    // On submit click
+    submit.addEventListener('click', function () {
+        // Do not submit if the message is empty
+        if (isBlank(message.value)) {
+            return;
         }
-    };
-});
 
-// On submit click
-submit.addEventListener('click', function () {
-    // Do not submit if the message is empty
-    if (isBlank(message.value)) {
-        return;
+        // Scroll to indicator
+        smoothScrollTo('loading');
+
+        // Submit form
+        onClickChange();
+    });
+
+    // On #chat_log_id select change
+    chat_log_id.addEventListener('change', function () {
+        // Do not submit if the message is empty
+        if (isBlank(chat_log_id.value)) {
+            return;
+        }
+
+        // Submit form
+        onClickChange();
+
+        // Clear chat
+        clearChat();
+    });
+
+    // On page load focus on the textarea #prompt
+    window.onload = function () {
+        message.focus();
     }
-
-    // Scroll to indicator
-    smoothScrollTo('loading');
-
-    // Submit form
-    onClickChange();
-});
-
-// On #chat_log_id select change
-chat_log_id.addEventListener('change', function () {
-    // Do not submit if the message is empty
-    if (isBlank(chat_log_id.value)) {
-        return;
-    }
-
-    // Submit form
-    onClickChange();
-
-    // Clear chat
-    clearChat();
-});
-
-// On page load focus on the textarea #prompt
-window.onload = function () {
-    message.focus();
 }
