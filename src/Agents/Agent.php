83c91cbe81595c28eef1ca233277f5af2cfab9b8
Carmelo Santana <me@carmelosantana.com>
2024-02-19 10:03:21 -0500

    Change alpaca_bot* to Options::prefixUnderscore

--- src/Agents/Agent.php
+++ src/Agents/Agent.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace CarmeloSantana\AlpacaBot\Agents;
 
+use CarmeloSantana\AlpacaBot\Options;
+
 abstract class Agent implements AgentInterface
 {
     private string $name;
@@ -15,7 +17,7 @@ abstract class Agent implements AgentInterface
 
     public function init()
     {
-        add_filter('alpaca_bot_core_agents', [$this, 'schema']);
+        add_filter(Options::prefixUnderscore('core_agents'), [$this, 'schema']);
     }
 
     public function job($args = [], $content = ''): string
