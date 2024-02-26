2bd1da602af8327de85e7f58c54cb9680f9ee425
Carmelo Santana <me@carmelosantana.com>
2024-02-26 17:35:56 -0500

    Bump version

--- src/AlpacaBot.php
+++ src/AlpacaBot.php
@@ -7,7 +7,7 @@ namespace CarmeloSantana\AlpacaBot;
 use CarmeloSantana\AlpacaBot\Define;
 use CarmeloSantana\AlpacaBot\Utils\Options;
 
-const VERSION = '0.4.3';
+const VERSION = '0.4.4';
 
 class AlpacaBot
 {
