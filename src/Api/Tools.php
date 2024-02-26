ad0b57c4c770225851fbf39a99639f9a8b25bb6f
Carmelo Santana <me@carmelosantana.com>
2024-02-26 17:38:03 -0500

    Add modify Authorization header helper method

---
+++ src/Api/Tools.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CarmeloSantana\AlpacaBot\Api;
+
+use CarmeloSantana\AlpacaBot\Utils\Options;
+
+class Tools
+{
+    public static function addAuth($args)
+    {
+        $username = Options::get('api_username');
+        $password = Options::get('api_password');
+
+        if ($username and $password) {
+            $args['headers']['Authorization'] = 'Basic ' . base64_encode($username . ':' . $password);
+        }
+
+        return $args;
+    }
+}
